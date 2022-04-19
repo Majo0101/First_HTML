36c958ede97c4ea1a7525f273548a71acc24d6f6
Pondelok <bodnar.marian@gmail.com>
2022-04-19 23:30:08 +0200

    Update

--- js/photos.js
+++ js/photos.js
@@ -4,7 +4,6 @@ function left(int) {
 
     const gallery = ["gallery/1.jpg", "gallery/2.jpg", "gallery/3.jpg", "gallery/4.jpg" ,"gallery/5.jpg"];
 
-
     if (int === 1) {
         store += 1;
         if (store > 4){
